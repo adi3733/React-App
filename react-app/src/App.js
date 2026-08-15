90f951047c9b619a0c0ab762dc0ff1c9331a2689
adi3733 <adi.ghayal23@gmail.com>
2026-08-15 23:14:30 +0530

    first commit

---
+++ react-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is my first React Application
+          Name:- Aditya Ravindra Ghayal
+          Sub:- Cloud Computing
+          Exp Aim :- Design and deploy a PaaS environment.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
